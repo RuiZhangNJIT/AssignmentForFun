7e046af7a5a1d3daf1f721863c2193381145cc37
Rui Zhang <rz237@njit.edu>
2020-02-03 07:04:37 -0800

    Update DansDilemma.java

--- src/main/java/DansDilemma/DansDilemma.java
+++ src/main/java/DansDilemma/DansDilemma.java
@@ -1,16 +1,57 @@
 package DansDilemma;
 
+import java.util.*;
+
 public class DansDilemma {
+	
+	public void addTwo(Double input1, Double input2,Set<Double> set)
+	{
+		set.add((double)(input1/input2));
+        set.add((double)(input2/input1));
+        set.add((double)(input1+input2));
+        set.add((double)(input1*input2));
+        set.add((double)(input1-input2));
+        set.add((double)(input2-input1));
+        
+	}
 
     public Integer dilemmaOfTwo(Double input1, Double input2) {
-        return null;
+        Set<Double> set=new HashSet<Double>();
+        set.add((double)(input1/input2));
+        set.add((double)(input2/input1));
+        set.add((double)(input1+input2));
+        set.add((double)(input1*input2));
+        set.add((double)(input1-input2));
+        set.add((double)(input2-input1));
+        
+        return set.size();
+        
     }
 
     public Integer dilemmaOfThree(Double input1, Double input2, Double input3) {
-        return null;
+        Double[] input_array= {input1,input2,input3};
+        Set<Double> set=new HashSet<Double>();
+        
+        for(int i=0;i<3;++i)
+        {
+        	for(int j=i+1;j<3;++j)
+        	addTwo(input_array[i],input_array[j],set);
+        }
+        
+        
+        return set.size();
     }
 
     public Integer dilemmaOfN(Double... args){
-        return null;
+        
+           Set<Double> set=new HashSet<Double>();
+           
+           for(int i=0;i<args.length;++i)
+        	   for(int j=i+1;j<args.length;++j)
+        	   {
+        		   addTwo(args[i],args[j],set);
+        	   }
+           
+           return set;
     }
 }
