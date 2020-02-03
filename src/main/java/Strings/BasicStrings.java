30f1415560ae00fed04f47f02e1e067d12e18b71
Rui Zhang <rz237@njit.edu>
2020-02-03 07:03:59 -0800

    Update BasicStrings.java

--- src/main/java/Strings/BasicStrings.java
+++ src/main/java/Strings/BasicStrings.java
@@ -3,26 +3,77 @@ package Strings;
 public class BasicStrings {
 
     public String flipConcat(String string1, String string2) {
-        return null;
+        
+    	return string1+string2;
     }
 
     public char getChar(String string, int index) {
-        return ' ';
+        return string.charAt(index);
     }
 
     public String iCantSee(String string) {
-        return null;
+        String ret="";
+        for(int i=0;i<string.length();++i)
+        	ret+=" ";
+        
+        return ret;
     }
 
     public String loudAndClear(String string) {
-        return null;
+        String str="";
+        for(int i=0;i<string.length();++i)
+        {
+        	if(Character.isUpperCase(string.charAt(i)))
+        	{
+        		str+=string.charAt(i);
+        	}
+        	else
+        	{
+        		str+=Character.toUpperCase(string.charAt(i));
+        	}
+        }
+        
+        return str;
     }
 
     public String reverseCase(String string) {
-        return null;
+          String ret="";
+          for(int i=0;i<string.length();++i)
+          {
+        	  if(Character.isUpperCase(string.charAt(i)))
+        	  {
+        		  ret+=Character.toLowerCase(string.charAt(i));
+        	  }
+        	  else
+        	  {
+        		  ret+=Character.toUpperCase(string.charAt(i));
+        		  
+        	  }
+          }
+          
+          return ret;
     }
 
     public String oneAtATime(String string1, String string2) {
-        return null;
+        String ret="";
+        int ptr_str1=0;
+        int ptr_str2=0;
+        
+        
+        while(ptr_str1<string1.length() && ptr_str2<string2.length())
+        {
+        	if(ptr_str1<=ptr_str2)
+        	{
+        		ret+=string1.charAt(ptr_str1);
+        		ptr_str1++;
+        	}
+        	else
+        	{
+        		ret+=string2.charAt(ptr_str2);
+        		ptr_str2++;
+        	}
+        }
+        
+        return ret;
     }
 }
